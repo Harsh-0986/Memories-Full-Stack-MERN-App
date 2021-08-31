7993120df7a62437c410df1bee94c5cd912bfe9f
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-08-31 10:14:50 +0530

    Connected to Database

--- server/index.js
+++ server/index.js
@@ -2,4 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
 // 8780903282 -> Rytham
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://hrsh0986:Hrsh2125@cluster0.lrobn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+  .catch((error) => console.log(error.message));
