a1e7d131f108c22134a8f75eba0f211a9d0dd5d2
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-08-31 10:27:03 +0530

    Add Routes to server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 // 8780903282 -> Rytham
 
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
