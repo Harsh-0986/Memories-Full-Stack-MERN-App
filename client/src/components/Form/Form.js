e44e0dacc4a6be88fe30c30cce5de578481ae1af
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-01 09:37:40 +0530

    Creating a Post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
 
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -12,8 +14,13 @@ const Form = () => {
     selectedFile: "",
   });
   const classes = useStyles();
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createPost(postData));
+  };
 
   const clear = () => {};
 
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,7 +2,8 @@ export default (posts = [], action) => {
   switch (action.type) {
     case "FETCH_ALL":
       return action.payload;
-
+    case "CREATE":
+      return [...posts, action.payload];
     default:
       return posts;
   }
