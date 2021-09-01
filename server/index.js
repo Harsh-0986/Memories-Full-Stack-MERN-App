35e63f2a9bc9c74813f33282b9a35bf4f9e3583f
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-01 09:00:17 +0530

    Implementing redux

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://hrsh0986:Hrsh2125@cluster0.lrobn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
