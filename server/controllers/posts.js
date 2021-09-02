6cfa0f269cf4247e505b47ebb022e9a796efd6c2
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-02 18:41:43 +0530

    Deploying frontend to netlify

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-import PostMessage from "../models/PostMessage.js";
+import PostMessage from "../models/postMessage/index.js";
 import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
