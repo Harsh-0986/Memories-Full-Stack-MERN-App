42844552a83872fb1814e6c647eefa76e38bf288
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-09-13 10:38:40 +0530

    Added routes on frontend and added pagination structure

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Auth from "./components/Auth/Auth";
 import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
     <BrowserRouter>
       <Container maxwidth="xl">
@@ -18,7 +20,10 @@ const App = () => {
           <Route path="/posts" exact component={Home} />
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/:id" component={PostDetails} />
-          <Route path="/auth" component={Auth} />
+          <Route
+            path="/auth"
+            component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+          />
         </Switch>
       </Container>
     </BrowserRouter>
